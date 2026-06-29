f6fc4240ca9f92c3da2e8eab903a1403a52cdb37
MD Usman Khan <usmankhan127w@gmail.com>
2026-06-30 01:14:39 +0500

    Implement user login with Firebase

---
+++ js/login.js
@@ -0,0 +1,29 @@
+import { auth } from "./firebaseConfig.js";
+
+import {
+  signInWithEmailAndPassword
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+
+const loginBtn = document.getElementById("loginBtn");
+
+loginBtn.addEventListener("click", async () => {
+
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+
+    try {
+
+        await signInWithEmailAndPassword(
+            auth,
+            email,
+            password
+        );
+
+        alert("Login Successful");
+
+        window.location.href = "home.html";
+
+    } catch (error) {
+        alert(error.message);
+    }
+});
